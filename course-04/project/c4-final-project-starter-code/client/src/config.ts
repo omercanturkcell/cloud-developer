64e672a55a782de3043d5e4d270cf213ee0b5e2c
Omercan Yuksel <omercan.yuksel@turkcell.com.tr>
2020-06-14 14:43:06 +0300

    get All Todos

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,9 +1,8 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
+
 const apiId = 'ynkne2ednl'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map
   domain: 'dev-ba5ouksr.us.auth0.com',            // Auth0 domain
   clientId: 'uZdg7I7GVcSx0HlUsLKpnlE6NNT15UeN',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
